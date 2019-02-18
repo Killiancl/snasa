43c74436c329d9c85d9d0b27e32c943b484be799
Killian Chopardlallier <killiancl64@gmail.com>
2019-02-18 18:31:28 +0100

    Add routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'spaceships#index'
+  resources :spaceships, only: [:index, :show] do
+    resources :bookings, only: [:new, :create, :show, :index]
+
+  end
+  resources :bookings, only: [:new, :create] do
+    resources :reviews, only: [:new, :create, :show, :index]
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
