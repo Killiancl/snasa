07a03fcf78329a0e2c95f49b780b2f31c99d1d9b
CyrilDebon <cyril.debon@gmail.com>
2019-02-21 17:49:58 +0100

    add carroussel to home

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,13 @@ vador = User.create!(first_name:"Vador", last_name:"dark", email:"vador@gmail.co
 
 
 
-Spaceship.create!(title: "Millenium Falcon", description:"Legendary Han and Chewie's spaceship", seat:"12", price_per_day: 50, address: "34 cours Edouard Vaillant", photo: "http://www.igorstshirts.com/blog/conceptships/fred_bastide.jpg" , user: toto)
+Spaceship.create!(title: "Millenium Falcon", description:"Legendary Han and Chewie's spaceship", seat:"12", price_per_day: 50, address: "34 cours Edouard Vaillant", photo: "https://publigeekaire.com/wp-content/uploads/2015/09/faucon-millenium.jpg" , user: toto)
 
 
-Spaceship.create!(title: "Nemesis", description:"", seat:"12", price_per_day: 50, address: "34 cours Edouard Vaillant", photo: "https://images.pexels.com/photos/2159/flight-sky-earth-space.jpg?auto=compress&cs=tinysrgb&h=650&w=940
-https://images.pexels.com/photos/2159/flight-sky-earth-space.jpg?auto=compress&cs=tinysrgb&h=650&w=940
-1x, https://images.pexels.com/photos/2159/flight-sky-earth-space.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", user: toto)
+Spaceship.create!(title: "Nemesis", description:"", seat:"12", price_per_day: 50, address: "34 cours Edouard Vaillant", photo: "https://static3.srcdn.com/wordpress/wp-content/uploads/2018/11/Amazing-Star-Citizen-Spaceship-in-Asteroids.jpg", user: toto)
 
 
-Spaceship.create!(title: "Ariane", description:"", seat:"6", price_per_day: 200, address: "145 cours Alsace-Lorraine", photo: "https://3dexport.com/items/2013/06/18/274615/71288/spaceship_snail_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_762549_o.jpg" , user: toto)
+Spaceship.create!(title: "Ariane", description:"", seat:"6", price_per_day: 200, address: "145 cours Alsace-Lorraine", photo: "https://www.usinenouvelle.com/mediatheque/4/9/6/000632694/ariane-6-airbus.jpg" , user: toto)
 
 
-Spaceship.create!(title: "Soyouz", description:"", seat:"2", price_per_day: 25, address: "78 rue Baudin", photo: "https://cdn.drawception.com/images/panels/2015/3-2/h3ByDPZmm3-6.png", user: vador)
+Spaceship.create!(title: "Soyouz", description:"", seat:"2", price_per_day: 25, address: "78 rue Baudin", photo: "https://media.ouest-france.fr/v1/pictures/260b4457710c1b23f7e33693913a5c41-le-nouveau-satellite-espion-francais-lance-par-une-soyouz-russe.jpg?width=1260&height=712&fill=0&focuspoint=74%2C46&cropresize=1&client_id=cmsfront&sign=d8f8ec4b318f28201bacb4507b9a4b894221ab8f164c8659383116c5bc862216", user: vador)
